83e08668f920ba0c9eca385ce0c6f11c76b7d4e6
lby <344818834@qq.com>
2018-11-20 14:12:08 +0800

    day2 自己

---
+++ market/supermarket/forms.py
@@ -0,0 +1,67 @@
+from django import forms
+from django.core import validators
+
+from supermarket.models import Users
+
+#注册表单
+class RegForm(forms.ModelForm):
+    pwd2 = forms.CharField(max_length=16,
+                           min_length=6,
+                           label="确认密码",
+                           strip=True,
+                           error_messages={
+                               "required": "请填写重复密码"
+                           }
+                           )
+    class Meta:
+        model = Users
+        exclude = ['isDelete']
+        widgets = {
+            "mobile": forms.TextInput(attrs={'class': "form-control"}),
+            "password": forms.PasswordInput(attrs={'class': "form-control"}),
+        }
+        labels = {
+            "mobile": "手机号",
+            "password": "密码",
+        }
+        error_messages = {
+            "mobile": {'required': '手机号不能为空!'},
+            "password": {'required': '密码不能为空!', 'min_length': "密码长度必须大于6", 'max_length': "密码长度必须小于16"},
+        }
+
+    def clean_mobile(self):
+        mobile = self.cleaned_data.get('mobile')
+        rs = Users.objects.filter(mobile=mobile).exists()
+        if rs:
+            raise forms.ValidationError("手机号已被注册,请换个!")
+        else:
+            return mobile
+
+    def __init__(self, *args, **kwargs):
+        super(RegForm, self).__init__(*args, **kwargs)
+        self.fields['mobile'].validators.append(validators.RegexValidator(r'^1[3-9]\d{9}$',"手机号码格式错误"))
+
+    def clean(self):
+        cleaned_data = super().clean()
+        pwd1 = cleaned_data.get('password')
+        pwd2 = cleaned_data.get('pwd2')
+        if pwd1 != pwd2:
+            raise forms.ValidationError({"pwd2": "两次密码不一致!"})
+        else:
+            if all((pwd1, pwd2)):
+                cleaned_data['password'] = pwd
+            return cleaned_data
+
+#登录表单
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = Users
+        fields = ['mobile', 'password']
+        labels = {
+            "mobile": "手机号",
+            "password": "密码",
+        }
+        error_messages = {
+            'mobile': {'required': '手机号不能为空^_^'},
+            'password': {'required': '密码不能为空'},
+        }
--- market/supermarket/views.py
+++ market/supermarket/views.py
@@ -1,45 +1,116 @@
-
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-
 # Create your views here.
-#登录界面
+
+from supermarket.forms import RegForm, LoginForm
 from supermarket.models import Users
 
 
+# 登录界面
+
+# def login(request):
+#     if request.method == "POST":
+#         #完成登录验证
+#         mobile = request.POST.get("mobile")
+#         password = request.POST.get("password")
+#         try:
+#             user = Users.objects.get(mobile = mobile)
+#         except Users.MultipleObjectsReturned:
+#             #获取多个记录
+#             return redirect("supermarket:login")
+#         except Users.DoesNotExist:
+#             return redirect("supermarket:login")
+#         #判断密码是否正确
+#         if password != user.password:
+#             return redirect("supermarket:login")
+#         #登陆成功
+#         else:
+#             return redirect("supermarket:index")
+#     else:
+#         return render(request, '用户/login.html')
+# 登录
 def login(request):
-    if request.method == "POST":
-        #完成登录验证
-        mobile = request.POST.get("mobile")
-        password = request.POST.get("password")
-        try:
-            user = Users.objects.get(mobile = mobile)
-        except Users.MultipleObjectsReturned:
-            #获取多个记录
-            return redirect("supermarket:login")
-        except Users.DoesNotExist:
-            return redirect("supermarket:login")
-        #判断密码是否正确
-        if password != user.password:
-            return redirect("supermarket:login")
-        #登陆成功
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            mobile = data.get('mobile')
+            password = data.get('password')
+            try:
+                user = Users.objects.get(mobile=mobile)
+            except Users.MultipleObjectsReturned:
+                return redirect("supermarket:login")
+            except Users.DoesNotExist:
+                return redirect("supermarket:login")
+            if password == user.password:
+                request.session['ID'] = user.pk
+                return redirect("supermarket:index")
+            else:
+                form.errors['password'] = ['密码错误!']
+
         else:
-            return redirect("supermarket:index")
-    else:
-        return render(request,'login.html')
-#注册界面
+            context = {
+                "errors": form.errors
+            }
+            return render(request, "用户/login.html", context)
+    return render(request, "用户/login.html")
+
+
+# 注册界面
 def reg(request):
-    #接收数据
+    # 接收数据
     if request.method == 'POST':
         data = request.POST
-        mobile = data.get('mobile')
-        password = data.get('password')
-        #处理数据
-        Users.objects.create(mobile=mobile,password = password)
-        return redirect("supermarket:login")
+        # 创建form对象
+        form = RegForm(data)
+        if form.is_valid():
+            data = form.cleaned_data
+            # 保存到数据库
+            mobile = data.get('mobile')
+            password = data.get('password')
+            # 处理数据
+            Users.objects.create(mobile=mobile, password=password)
+            return redirect("supermarket:login")
+        else:
+            context = {
+                "errors": form.errors
+            }
+            return render(request, "用户/reg.html", context)
     else:
-        return render(request, 'reg.html')
-#首页
+        return render(request, '用户/reg.html')
+
+
+# 修改密码
+def forgetpassword(request):
+    return render(request, '用户/forgetpassword.html')
+
+
+# 首页
 def index(request):
-    return render(request,'login.html')
+    return render(request, 'index.html')
+
+
+# 个人中心
+
+def member(request):
+    return render(request, '用户/member.html')
+
+
+#全部订单
+def allorder(request):
+    return render(request, 'allorder.html')
+
+#购物车
+
+def shopcar(request):
+    return render(request, 'shopcar.html')
+
+
+#动态
+def message(request):
+    return render(request, 'message.html')
+
+#收货地址
+def gladdress(request):
+    return render(request,'用户/gladdress.html')
